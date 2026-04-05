brianwiddas/meshcore-pi

commit 43eb3d93da10f9ec15332d27fce37e4fa204c01c

Fix MeshTNC interface bug
When a packet it sent to the MeshTNC device for transmission, the device
responds with the packet data. We check for that to distinguish between the
response to a command and response to a packet transmission. However, the
initial value of the variable holding this data was a blank string, so it
incorrectly matched everything until the first packet was sent.

The initial value is now 'NOPACKET'

interfaces/meshtnc.py
@@ -44,7 +44,11 @@ class MeshTNC(Interface):
 
         self._connected = False
 
-        self.lastpacket = b''
+        # Record the last packet sent, so we know if it comes back to us
+        # Can't initialise to an empty string, or it will match all packets in
+        # startswith() below
+
+        self.lastpacket = b'NOPACKET'
 
         # Fetch all the config we need
         # Default config is UK/EU Narrow
@@ -134,10 +138,12 @@ class MeshTNC(Interface):
                         logger.warning(f"Error decoding RXLOG line from MeshTNC: {line.decode(errors='replace')}, {repr(e)}")
                         continue
 
+                    print("Data:", hexlify(data))
                     # Add the packet to the receive queue
                     await self.rx_q.put((data, rssi, snr))
 
                 else:
+                    print("Command response")
                     # Command response, put it in the CLI response queue
                     await self._rx_cli.put(line)
         except Exception as e:

commit bf84dfa40db92c3feb0f097617476e13a17333b5

Remove debug prints
43eb3d93 (Fix MeshTNC interface bug) accidentally included a couple of print
statements which were used for debugging. They're harmless, but untidy.

interfaces/meshtnc.py
@@ -138,12 +138,10 @@ class MeshTNC(Interface):
                         logger.warning(f"Error decoding RXLOG line from MeshTNC: {line.decode(errors='replace')}, {repr(e)}")
                         continue
 
-                    print("Data:", hexlify(data))
                     # Add the packet to the receive queue
                     await self.rx_q.put((data, rssi, snr))
 
                 else:
-                    print("Command response")
                     # Command response, put it in the CLI response queue
                     await self._rx_cli.put(line)
         except Exception as e: